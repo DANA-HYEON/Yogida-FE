cdf0c7bf068408ed32305b20d0c9c82cf275b329
Dahyeon Lee <dada4202@gmail.com>
2024-01-15 10:25:32 +0900

    fix: 백엔드 url 수정

--- vite.config.js
+++ vite.config.js
@@ -7,8 +7,8 @@ export default defineConfig({
   plugins: [react(), svgr()],
   server: {
     proxy: {
-      '/api/v1': 'http://localhost:8080',
-      '/images': 'http://localhost:8080',
+      '/api/v1': 'http://yogida-env-1.eba-zrumtkkz.ap-northeast-2.elasticbeanstalk.com',
+      '/images': 'http://yogida-env-1.eba-zrumtkkz.ap-northeast-2.elasticbeanstalk.com',
     },
   },
 });
